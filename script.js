23c8eb312ebbe1b03cedba56bc8dc69c4dc681e9
A <abhishekkumarsingh9585@gmail.com>
2025-04-02 00:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function firstWord(s) {
   // your code here
-  if(s.length==0){
+	  if(s.length==0){
     return "";
   }
   let str=s.split(" ");
   return str[0].join("");
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
